6f0871da13571b99a97f92ac82463365db4dc10b
pang-vichhai <pangvichhai@gmail.com>
2023-01-26 14:55:09 +0700

    add next and pre to paginated

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,13 +1,15 @@
 class BlogsController < ApplicationController
     before_action :set_blog, only: [:show, :update, :destroy]
+    include Paginable
 
-    def index 
-        @blogs = Blog.recent 
-        # render json: @blogs
-        render json: serializer.new(@blogs)
+    def index      
+        paginated = paginate(Blog.recent) 
+        render_collection(paginated)
     end
 
     def serializer 
         BlogSerializer
     end
+
+
 end
