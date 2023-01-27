11c97b6f46706dd76adaf9115b494a8471eacedb
pang-vichhai <pangvichhai@gmail.com>
2023-01-27 15:29:31 +0700

    add user auth

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'jsom-pagination'
 
 gem 'jsonapi_errors_handler'
 
+gem "octokit", "~> 4.0"
+
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 gem 'rack-cors'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_25_084207) do
+ActiveRecord::Schema.define(version: 2023_01_26_093104) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(version: 2023_01_25_084207) do
   create_table "infos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
     t.text "desc"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "login", null: false
+    t.string "name"
+    t.string "url"
+    t.string "avatar_url"
+    t.string "provider"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
